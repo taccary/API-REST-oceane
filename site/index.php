8dcb8acc73d72f00adeb32766470fb0816039647
Tiphaine Accary-Barbier <34951152+taccary@users.noreply.github.com>
2025-09-16 12:01:30 +0000

    Base TP

--- site/index.php
+++ site/index.php
@@ -1,15 +1,8 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Authorization, Content-Type");
-    http_response_code(200);
-    exit;
-}
 header("Content-Type: application/json");
 include 'configBdd.php';
 require __DIR__ . '/API/bateau.php';
-require __DIR__ . '/API/authentification.php';
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
@@ -21,40 +14,12 @@ $resultat = null;
 if ($segments[0] === 'bateaux') {
     switch ($method) {
         case 'GET':
-            if (isset($segments[1]) && is_numeric($segments[1])) {
-                $resultat = getBateau($pdo, intval($segments[1]));
-            } else {
-                $resultat = getBateaux($pdo);
-            }
-            break;
-        case 'POST':
-            $user = checkJWT();
-            if (!$user) {
-                http_response_code(401);
-                return json_encode(["error" => "Non autorisé"]);
-            }
-            $resultat = createBateau($pdo, json_decode(file_get_contents('php://input'), true));
-            break;
-        case 'PUT':
-            if (isset($segments[1]) && is_numeric($segments[1])) {
-                $resultat = updateBateau($pdo, intval($segments[1]), json_decode(file_get_contents('php://input'), true));
-            } else {
-                $resultat = json_encode(["status" => "error", "message" => "ID manquant"]);
-            }
-            break;
-        case 'DELETE':
-            if (isset($segments[1]) && is_numeric($segments[1])) {
-                $resultat = deleteBateau($pdo, intval($segments[1]));
-            } else {
-                $resultat = json_encode(["status" => "error", "message" => "ID manquant"]);
-            }
+            $resultat = getBateaux($pdo);
             break;
         default:
             $resultat = json_encode(["status" => "error", "message" => "Méthode non autorisée"]);
     }
-} elseif ($segments[0] === 'login' && $method === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    $resultat = login($pdo, $data);
 }
+
 
 echo $resultat;
